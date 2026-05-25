b2b51670314b73cd818134531bece8a0230de0a7
dosedaf <mash1o1o1o1@gmail.com>
2026-05-25 11:05:42 +0700

    edit

--- utils/cache.go
+++ utils/cache.go
@@ -16,14 +16,12 @@ type MemoryCache struct {
 	items map[string]cacheItem
 }
 
-// NewMemoryCache membuat instance cache baru
 func NewMemoryCache() *MemoryCache {
 	return &MemoryCache{
 		items: make(map[string]cacheItem),
 	}
 }
 
-// Set menyimpan data ke dalam memori dengan batas waktu tertentu
 func (m *MemoryCache) Set(key string, value []byte, duration time.Duration) {
 	m.mu.Lock()
 	defer m.mu.Unlock()
@@ -34,7 +32,6 @@ func (m *MemoryCache) Set(key string, value []byte, duration time.Duration) {
 	}
 }
 
-// Get mengambil data dari memori. Mengembalikan false jika tidak ada atau sudah expired
 func (m *MemoryCache) Get(key string) ([]byte, bool) {
 	m.mu.RLock()
 	defer m.mu.RUnlock()
