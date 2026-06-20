5f118834cebc93ac7d3508cf4bcf1dc8ff2917df
dosedaf <mash1o1o1o1@gmail.com>
2026-06-20 20:26:22 +0700

    redeploy

--- models/models.go
+++ models/models.go
@@ -5,7 +5,7 @@ import "time"
 type User struct {
 	ID           uint      `gorm:"primaryKey" json:"id"`
 	Name         string    `json:"name"`
-	Email        string    `gorm:"uniqueIndex" json:"email"`
+	Email        string    `gorm:"size:191;uniqueIndex" json:"email"`
 	Password     string    `json:"-"`
 	Role         string    `gorm:"default:'USER'" json:"role"`
 	PhoneNumber  string    `json:"phoneNumber"`
